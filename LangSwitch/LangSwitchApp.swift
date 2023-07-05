56532f859325f2263733e204d06d2b9c40dfd31f
Anton Nikeev <antnick45@gmail.com>
2023-07-05 12:03:00 +0600

    Initial Commit

---
+++ LangSwitch/LangSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangSwitchApp.swift
+//  LangSwitch
+//
+//  Created by ANTON NIKEEV on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LangSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
